060b221ccb6d941c39b7efee0fa7a5d92690353c
Viggo Wozniak <vmpwozniak@gmail.com>
2024-09-03 19:00:42 -0600

    Completed control unit

--- riscvsingle.srcs/sources_1/new/controlunit.v
+++ riscvsingle.srcs/sources_1/new/controlunit.v
@@ -6,12 +6,13 @@
 // Create Date: 09/01/2024 04:13:20 PM
 // Design Name: 
 // Module Name: controlunit
-// Project Name: 
+// Project Name: riscvsingle
 // Target Devices: 
 // Tool Versions: 
-// Description: Combines 
+// Description: Combines the various decoders used for generating control signals into one unit.
+//              Outputs all control signals used in the data path.
 // 
-// Dependencies: 
+// Dependencies: maindecoder.v, ALUdecoder.v, branchdecoder.v, and widthdecoder.v
 // 
 // Revision:
 // Revision 0.01 - File Created
@@ -23,10 +24,30 @@
 module controlunit(input [6:0] op,
                    input [2:0] funct3,
                    input funct7b5,
+                   input N, Z, C, V,
                    output [3:0] ALUControl,
                    output [2:0] ImmSrc, WidthSrc, ResultSrc,
                    output ALUSrc,
                    output RegWrite, MemWrite,
                    output PCSrc, PCBaseSrc);
         
-endmodule
+    
+    //Internal control signals
+    wire [1:0] ALUOp, BranchOp;
+    wire WidthOp;
+    
+    //Main Decoder
+    maindecoder MainDec(op, ImmSrc, ResultSrc, ALUOp, BranchOp, WidthOp, ALUSrc, PCBaseSrc, RegWrite, MemWrite);
+    
+    //ALU Decoder
+    ALUdecoder ALUDec(funct3, ALUOp, op[5], funct7b5, ALUControl);
+    
+    //Branch Decoder
+    branchdecoder BranchDec(funct3, BranchOp, N, Z, C, V, PCSrc);
+    
+    //Width Decoder
+    widthdecoder WidthDec(funct3, WidthOp, WidthSrc);        
+        
+        
+        
+endmodule   
